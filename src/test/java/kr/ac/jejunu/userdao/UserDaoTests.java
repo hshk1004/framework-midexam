be4fa6832243b7042e6154d12123587648be12eb
hshk1004 <hshk_1004@naver.com>
2021-04-30 00:52:11 +0900

    유저 추가 기능 만들기

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
@@ -22,14 +23,23 @@ public class UserDaoTests {
     }
 
     @Test
-    public void InsertUser() {
-        Integer id = 2;
-        String name = "qkqh";
-        String password = "3333";
-        UserDao userDao = new UesrDao();
-        User user = userDao.set(id);
-        assertThat(user.setId(), is(id));
-        assertThat(user.setName(), is(name));
-        assertThat(user.setPassword(), is(password));
+    public void InsertUser() throws SQLException, ClassNotFoundException {
+        //추가할 사용자 이름과 비번
+        String name = "whffu";
+        String password = "12345";
+
+        //유저의 이름과 비번 설정
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        assertThat(user.getId(), greaterThan(0));
+
+        User insertedUser = userDao.get(user.getId());
+        assertThat(insertedUser.getName(), is(name));
+        assertThat(insertedUser.getPassword(), is(password));
+
     }
 }
